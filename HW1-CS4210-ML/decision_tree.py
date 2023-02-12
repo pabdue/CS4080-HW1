22a65e7eb49d6aa6f569b6dc89ba0a8a721b1f14
Pablo Duenas <63692917+pabdue@users.noreply.github.com>
2023-02-11 22:37:40 -0800

    Add files via upload

---
+++ HW1-CS4210-ML/decision_tree.py
@@ -0,0 +1,55 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Pablo Duenas
+# FILENAME: decision_tree.py
+# SPECIFICATION: practice on decision trees
+# FOR: CS 4210- Assignment #1
+# TIME SPENT: the total assignment ~7 hours, the .py file ~ 1 Hour
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
+# dictionaries, lists, and arrays
+
+#importing some Python libraries
+from sklearn import tree
+import matplotlib.pyplot as plt
+import csv
+db = []
+X = []
+Y = []
+
+#reading the data in a csv file
+with open('contact_lens.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         db.append (row)
+         print(row)
+
+#transform the original categorical training features into numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
+# so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
+#--> add your Python code here
+for i in range(1, 11):
+    sub_list = [i, i, i, i]
+    X.append(sub_list)
+
+# X = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5], [6, 6, 6, 6], [7, 7, 7, 7], [8, 8, 8, 8], [9, 9, 9, 9], [10, 10, 10, 10]]
+
+#transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
+#--> addd your Python code here
+
+for i in db:
+  if i[4] == 'Yes':
+      Y.append(1)
+  else:
+      Y.append(2)
+
+# Y = [2, 2, 2, 1, 1, 1, 2, 2, 2, 1]
+
+#fitting the decision tree to the data
+clf = tree.DecisionTreeClassifier(criterion = 'entropy')
+clf = clf.fit(X, Y)
+
+#plotting the decision tree
+tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
+plt.show()
+
